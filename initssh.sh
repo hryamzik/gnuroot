0c8d58ec154ce33c7fa13743c58c980730ec1529
Роман Беляковский <ihryamzik@gmail.com>
2015-08-14 15:08:19 +0300

    removed PermitRootLogin from dropbear config in initssh script as it's an illigal option

--- initssh.sh
+++ initssh.sh
@@ -2,5 +2,4 @@ apt-get update
 apt-get install dropbear
 sed -Ei 's/^(DROPBEAR_PORT=22)$/\122/' /etc/default/dropbear
 sed -Ei 's/^(NO_START=)(1)/\10/' /etc/default/dropbear
-grep -q PermitRootLogin /etc/default/dropbear || echo "PermitRootLogin yes" >> /etc/default/dropbear
 /etc/init.d/dropbear start
